b2b3aa4db97b371f243042cf08dd774676c54505
Maikel Vreugde <git@mvesign.com>
2024-02-22 23:05:44 +0100

    Copy instructions of year 2015 day 4

---
+++ AdventOfCode.Year2015/Days/Day04/Instructions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode.Year2015.Days.Day04;
+
+public class Instructions(int year, int day) : Abstractions.Instructions(year, day)
+{
+    private string _input = string.Empty;
+
+    public override object PerformPartOne()
+        => Calculate(new string('0', 5));
+
+    public override object PerformPartTwo()
+        => Calculate(new string('0', 6));
+
+    protected override void LoadInput(string filePath)
+    {
+        _input = File.ReadAllText(filePath);
+    }
+
+    private int Calculate(string startingValue)
+    {
+        for (var count = 0;; count++)
+            if (
+                BitConverter.ToString(
+                    MD5.HashData(Encoding.UTF8.GetBytes($"{_input}{count}"))
+                )
+                .Replace("-", "")
+                .StartsWith(startingValue)
+            )
+                return count;
+    }
+}
