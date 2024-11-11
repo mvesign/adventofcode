8d0840eeef88d7c5983c5eb40fc4b60c4e09469b
Maikel Vreugde <git@mvesign.com>
2024-11-11 22:33:19 +0100

    Not much, but this is what I got for 2019, need a lot of work here

---
+++ AdventOfCode.Year2019/Days/Day01/Instructions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2019.Days.Day01;
+
+public class Instructions() : Abstractions.Instructions(year: 2019, day: 1)
+{
+    private decimal[] _requirements = [];
+
+    public override void LoadInput() =>
+        _requirements = ReadAllLines()
+            .Select(decimal.Parse)
+            .ToArray();
+
+    public override object PerformPartOne() =>
+        _requirements.Sum(requirement => Math.Floor(requirement / 3) - 2);
+
+    public override object PerformPartTwo() 
+    {
+        return _requirements.Sum(requirement => CalculateFuel(requirement).Sum());;
+
+        IEnumerable<decimal> CalculateFuel(decimal mass)
+        {
+            while (mass > 0)
+            {
+                mass = Math.Floor(mass / 3) - 2;
+                yield return mass > 0 ? mass : 0;
+            }
+        }
+    }
+}
---
+++ AdventOfCode.Year2019/Days/Day02/Instructions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2019.Days.Day02;
+
+public class Instructions() : Abstractions.Instructions(year: 2019, day: 2)
+{
+    private int[] _states = [];
+
+    public override void LoadInput() =>
+        _states = ReadAllText()
+            .Split(',')
+            .Select(int.Parse)
+            .ToArray();
+
+    public override object PerformPartOne()
+    {
+        _states[1] = 12;
+        _states[2] = 2;
+
+        ProcessIntcode(_states);
+
+        return _states[0];
+
+        static void ProcessIntcode(int[] codes)
+        {
+            for(var position = 0; position + 4 < codes.Length; position += 4)
+            {
+                var value = codes[position];
+                var noun = codes[position + 1];
+                var verb = codes[position + 2];
+                var result = codes[position + 3];
+
+                if (value == 99)
+                    return;
+
+                codes[result] = value == 1
+                    ? codes[noun] + codes[verb]
+                    : value == 2
+                        ? codes[noun] * codes[verb]
+                        : codes[result];
+            }
+        }
+    }
+
+    public override object PerformPartTwo() 
+    {
+        return 0;
+    }
+}
